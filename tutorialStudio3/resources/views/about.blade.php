306bfd27484812d66a28442c1dc6bba02f84137f
glwatkins <georgettewatkins1@gmail.com>
2021-03-11 23:03:21 +1300

    attempt to fix errors

---
+++ tutorialStudio3/resources/views/about.blade.php
@@ -0,0 +1,11 @@
+
+@extends('layout')
+
+@section('title')
+Laravel 5.8 Basics | About Page
+@endsection
+
+@section('content')
+    <h3>About Page</h3>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+@endsection
